2c8bfd497fc6e2992e0f6749674af2218f6aa275
G-Code <111627085+gcodeweb@users.noreply.github.com>
2024-01-12 22:36:06 +0700

    Add files via upload

---
+++ register_form.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="form-container">
+        <form action="" method="post">
+            <h3>Register Now</h3>
+            <input type="text" name="name" required placeholder="enter your name">
+            <input type="email" name="email" required placeholder="enter your email">
+            <input type="password" name="password" required placeholder="enter your password">
+            <input type="password" name="cpassword" required placeholder="confirm your password">
+            <select name="user type">
+                <option value="user">user</option>
+                <option value="admin">admin</option>
+            </select>
+            <input type="submit" name="submit" value="register now" class="form-btn">
+            <p>already have account? <a href="login_form.php">login now</a></p>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ user_page.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container position-relative">
+        <div class="content position-absolute top-50 start-50">
+            <h3>hi <span>user</span></h3>
+            <h1>welcome <span></span></h1>
+            <p>this is an admin page</p>
+            <a href="login_form.php" class="btn">login</a>
+            <a href="register_form.php" class="btn">register</a>
+            <a href="logout.php" class="btn">logout</a>
+        </div>
+    </div>
+</body>
+
+</html>
